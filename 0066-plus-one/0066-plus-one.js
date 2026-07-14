d057ad55602dc5dc788f9d853ea60c6815657cd6
joaogcardoso <joaogabrielsc03012007@gmail.com>
2026-07-14 19:59:47 -0300

    0066-PLUS-ONE Stats: Time: 0 ms (100%), Space: 53.6 MB (61.11%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -6,13 +6,9 @@ var plusOne = function(digits) {
     let string = "";
     for (let i = 0; i < digits.length; i++) {
         string += String(digits[i]);
-        console.log(`String: ${string}`);
     }
     let num = BigInt(string);
-    console.log(num);
     num++;
-    console.log(num);
     const array = Array.from(String(num), Number);
-    console.log(array);
     return array;
 };
